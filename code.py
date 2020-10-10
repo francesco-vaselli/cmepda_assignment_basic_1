ebee8a543c5d4e15291f67e1113444f6464073dc
francesco-vaselli <frankvas98@gmail.com>
2020-10-10 22:07:06 +0200

    Added optional stats argument and empty test file

--- code.py
+++ code.py
@@ -9,16 +9,34 @@ import matplotlib.pyplot as plt
 
 logging.basicConfig(level=logging.WARNING)
 
-def process(file_path, bool):
+def process(file_path, hist_bool, stats_bool):
     """read text file and compile statistics
     """
     start_time = time.time()
     logging.info("Reading input file %s...", file_path)
-    #with keyword and context manager
-    with open(file_path) as input_file:
-        text = input_file.read()
-    num_chars = len(text)
-    logging.info("Done. Number of characters is %d", num_chars)
+    #open file with keyword and context manager
+    with open(file_path, 'r') as input_file:
+        #set pointer at the end of file, then check current postion to assess
+        #whether the file is empty (all without actually reading!!!)
+        input_file.seek(0, 2)
+        if input_file.tell() == 0:
+            print('FILE IS EMPTY')
+            return
+        else:
+            #reset counter to beginning of file
+            input_file.seek(0)
+            text = input_file.read()
+            #work on statistics if required
+            if stats_bool is True:
+                input_file.seek(0)
+                num_lines = sum(1 for line in input_file)
+                num_chars = len(text)
+                num_words = len(text.split())
+                logging.info(num_words)
+                logging.info(num_lines)
+
+
+    logging.info("Done.")
 
     char_dict = {ch: 0 for ch in string.ascii_lowercase}
     #store counters as dictonary values
@@ -33,7 +51,11 @@ def process(file_path, bool):
     for ch, num in char_rdict.items():
         print(f"{ch} -> {num:.3f}%")
 
-    if bool == False:
+    if stats_bool is True:
+        print(f"""The numer of characters is {num_chars}, the number of words is
+        {num_words}, while the number of lines is {num_lines}.""")
+
+    if hist_bool is False:
         elapsed_time = time.time() - start_time
         print(f"Done in {elapsed_time:.3f} seconds")
 
@@ -66,5 +88,8 @@ if __name__ == "__main__":
     #add optional argument for printing histogram
     parser.add_argument('-histogram', action='store_true',
                     help='Print histogram of the frequencies')
+    #add optional argument for printing stats
+    parser.add_argument('-stats', action='store_true',
+                    help='Print the basic book stats')
     args = parser.parse_args()
-    process(args.infile, args.histogram)
+    process(args.infile, args.histogram, args.stats)
